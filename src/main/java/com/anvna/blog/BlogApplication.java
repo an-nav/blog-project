998f3c82b313d8cb9ccb667ffeb0f0efdce4dbda
An_Vna <1297752084@qq.com>
2020-10-18 20:57:27 +0800

    搭建框架配置文件

--- src/main/java/com/anvna/blog/BlogApplication.java
+++ src/main/java/com/anvna/blog/BlogApplication.java
@@ -1,8 +1,12 @@
 package com.anvna.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Anvna
+ */
 @SpringBootApplication
 public class BlogApplication {
 
